bdd7538f24f68014913be0216dfd4b7462d6fe6c
diptowebhero <diptodas1672@gmail.com>
2021-11-24 01:18:20 +0600

    swper

--- src/Pages/Home/Testimonials/Testimonials.js
+++ src/Pages/Home/Testimonials/Testimonials.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import Testimonial from "../Testimonial/Testimonial";
 import "./Testimonial.css";
+import SwiperCore, { Autoplay, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
 import loader from "../../../assets/images/loader.gif";
+import 'swiper/swiper-bundle.min.css'
 const Testimonials = () => {
   const [reviews, setReviews] = useState([]);
-
+  SwiperCore.use([Pagination, Autoplay]);
   useEffect(() => {
     fetch("https://nameless-woodland-81515.herokuapp.com/review")
       .then((response) => response.json())
@@ -24,11 +27,39 @@ const Testimonials = () => {
               <img src={loader} alt="" />
             </div>
           ) : (
-            <Row xs={1} md={3} className="mx-auto pt-5">
-              {reviews?.map((testimonial) => (
-                <Testimonial key={testimonial._id} testimonial={testimonial} />
-              ))}
-            </Row>
+            <Swiper
+              loop={true}
+              pagination={{ clickable: true }}
+              slidesPerView={1}
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 2,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 10,
+                },
+                1024: {
+                  slidesPerView: 3,
+                  spaceBetween: 10,
+                },
+              }}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              spaceBetween={10}
+            >
+              <Row xs={1} md={3} className="mx-auto pt-5">
+                {reviews?.map((testimonial) => (
+                  <SwiperSlide key={testimonial._id}>
+                    {" "}
+                    <Testimonial testimonial={testimonial} />
+                  </SwiperSlide>
+                ))}
+              </Row>
+            </Swiper>
           )}
         </div>
       </Container>
